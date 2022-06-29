27cef94e3d5399e84bb83aff2bfc03b216b3853f
Amanda Hernandez <ahc13200@gmail.com>
2022-06-29 08:15:34 -0400

    Commit de modificacion del codigo

--- exportar.py
+++ exportar.py
@@ -1,8 +1,8 @@
-import buscar_similitud as buscaaar
+import buscar_similitud as buscar
 
 
 def exportar_resultados(requisitos_extraidos):
-    lista = buscaaar.similitudes(requisitos_extraidos)
+    lista = buscar.similitudes(requisitos_extraidos)
     archivo = open('requisitos_agrupados_similares.txt', 'w')
     for linea in lista:
         a = " ".join([str(_) + ", " for _ in linea])
